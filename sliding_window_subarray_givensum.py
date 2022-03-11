386f6141c0cb2407f20715f98266cf7f314c66b3
spurthirao999 <spurthirao999@gmail.com>
2022-03-10 20:32:46 -0800

    Update sliding_window_subarray_givensum.py

--- sliding_window_subarray_givensum.py
+++ sliding_window_subarray_givensum.py
@@ -1,4 +1,6 @@
 """
+https://practice.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1#
+
 Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
 
 In case of multiple subarrays, return the subarray which comes first on moving from left to right.
